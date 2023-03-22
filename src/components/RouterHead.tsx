085e824c3904ea24c15d0c9f2407127828eb58e4
Atte Liimatainen <atte.liimat@gmail.com>
2023-03-22 13:43:32 +0200

    add router head component
    - this will be used by each route to inject meta data

---
+++ src/components/RouterHead.tsx
@@ -0,0 +1,30 @@
+import { component$ } from '@builder.io/qwik';
+import { useDocumentHead } from '@builder.io/qwik-city';
+
+/**
+ * The RouterHead component is placed inside of the document `<head>` element.
+ */
+export const RouterHead = component$(() => {
+  const head = useDocumentHead();
+
+  return (
+    <>
+      <title>{head.title}</title>
+
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+
+      {head.meta.map((m, i) => (
+        <meta key={i} {...m} />
+      ))}
+
+      {head.links.map((l, i) => (
+        <link key={i} {...l} />
+      ))}
+
+      {head.styles.map((s, i) => (
+        <style key={i} {...s.props} dangerouslySetInnerHTML={s.style} />
+      ))}
+    </>
+  );
+});
--- src/root.tsx
+++ src/root.tsx
@@ -1,5 +1,6 @@
 import { component$ } from '@builder.io/qwik';
 import { QwikCityProvider, RouterOutlet, ServiceWorkerRegister } from '@builder.io/qwik-city';
+import { RouterHead } from './components/RouterHead';
 
 import './global.css';
 
@@ -7,6 +8,7 @@ export default component$(() => {
   return (
     <QwikCityProvider>
       <head>
+        <RouterHead />
         <meta charSet="utf-8" />
       </head>
       <body>
