58f9040dbde351875f2e7e222cf350646c36b1e8
Atte Liimatainen <atte.liimat@gmail.com>
2023-03-24 17:02:45 +0200

    possible fix to url not changing

--- adapters/static/vite.config.ts
+++ adapters/static/vite.config.ts
@@ -1,3 +1,5 @@
+import * as dotenv from 'dotenv'
+dotenv.config()
 import { staticAdapter } from '@builder.io/qwik-city/adapters/static/vite';
 import { extendConfig } from '@builder.io/qwik-city/vite';
 import baseConfig from '../../vite.config';
