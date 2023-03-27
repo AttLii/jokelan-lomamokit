9aa75589ec63a78d08ef3d6d0e18ad37bd329d0f
Atte Liimatainen <atte.liimat@gmail.com>
2023-03-27 21:28:51 +0300

    hide desktop nav on mobile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,9 +7,9 @@ type Props = {
 }
 export const Header = component$(({ menu: { title, menuItems } }: Props) => {
   return (
-    <header class="fixed w-full bg-slate-300 z-10">
+    <header class="fixed w-full bg-slate-300 z-10 h-6">
       <div class="max-w-7xl mx-auto px-2">
-        <nav aria-label={title}>
+        <nav class="hidden sm:block" aria-label={title}>
           <ul class="flex flex-wrap gap-x-4">
             {menuItems.map((menuItem, i) => (
               <li key={i}>
