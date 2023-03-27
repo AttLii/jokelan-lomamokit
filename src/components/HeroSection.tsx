b8a765654308066e6bde5e21c014e595a813722f
Atte Liimatainen <atte.liimat@gmail.com>
2023-03-27 20:36:39 +0300

    add component for RichText field

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,13 +1,17 @@
 import { component$ } from "@builder.io/qwik"
 import type { ParsedHero } from "~/parsers/contentful"
 import { Carousel } from "./Carousel"
+import { RichText } from "./RichText"
 
 type Props = ParsedHero
 export const HeroSection = component$(({ gallery, richText }: Props) => {
   return (
     <section class="relative overflow-hidden aspect-[9/16] sm:aspect-[16/9]">
       <Carousel images={gallery} />
-      <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white" dangerouslySetInnerHTML={richText} />
+      <RichText
+        _class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white"
+        dangerouslySetInnerHTML={richText}
+      />
     </section>
   )
 })
---
+++ src/components/RichText.tsx
@@ -0,0 +1,11 @@
+import { component$ } from "@builder.io/qwik"
+
+type Props = {
+  dangerouslySetInnerHTML: string;
+  _class?: string;
+}
+export const RichText = component$(({ _class, dangerouslySetInnerHTML }: Props) => {
+  return (
+    <div class={_class} dangerouslySetInnerHTML={dangerouslySetInnerHTML} />
+  )
+})
