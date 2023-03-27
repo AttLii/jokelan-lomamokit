f7988144fb599b4a08fafbd5684d880c8a0ecf90
Atte Liimatainen <atte.liimat@gmail.com>
2023-03-27 20:31:07 +0300

    add carousel component

---
+++ src/components/Carousel.tsx
@@ -0,0 +1,38 @@
+import { component$, useSignal, useVisibleTask$ } from "@builder.io/qwik";
+import type { ParsedImageAsset } from "~/parsers/contentful";
+import { AssetImage } from "./AssetImage";
+
+type Props = {
+  images: ParsedImageAsset[]
+}
+export const Carousel = component$(({ images }: Props) => {
+  const index = useSignal(0);
+  useVisibleTask$(() => {
+    const onInterval = () => {
+      if (index.value === images.length - 1) {
+        index.value = 0;
+      } else {
+        index.value++;
+      }
+    }
+    const timer = setInterval(onInterval, 5000);
+    return () => clearInterval(timer);
+  });
+  return (
+    <ul class="flex h-full transition-all duration-1000" style={{ transform: `translateX(${index.value === 0 ? 0 : index.value * 100 * -1}%)` }}>
+      {images.map(({ src, ...rest }, i) => (
+        <li class="min-w-full relative" key={i}>
+          <AssetImage
+            {...rest}
+            _class="w-full h-full object-cover object-center"
+            loading={i === 0 ? "eager" : "lazy"}
+            src={`${src}?w=639&h=1136`}
+            srcSet={{
+              '(min-width: 640px)': `${src}?w=1920&h=1080`
+            }}
+          />
+        </li>
+      ))}
+    </ul>
+  )
+})
--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,26 +1,12 @@
 import { component$ } from "@builder.io/qwik"
 import type { ParsedHero } from "~/parsers/contentful"
-import { AssetImage } from "./AssetImage"
+import { Carousel } from "./Carousel"
 
 type Props = ParsedHero
 export const HeroSection = component$(({ gallery, richText }: Props) => {
   return (
     <section class="relative overflow-hidden aspect-[9/16] sm:aspect-[16/9]">
-      <ul class="flex h-full">
-        {gallery.map(({ src, ...rest }, i) => (
-          <li class="min-w-full relative" key={i}>
-            <AssetImage
-              {...rest}
-              _class="w-full h-full object-cover object-center"
-              loading={i === 0 ? "eager" : "lazy"}
-              src={`${src}?w=639&h=1136`}
-              srcSet={{
-                '(min-width: 640px)': `${src}?w=1920&h=1080`
-              }}
-            />
-          </li>
-        ))}
-      </ul>
+      <Carousel images={gallery} />
       <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white" dangerouslySetInnerHTML={richText} />
     </section>
   )
