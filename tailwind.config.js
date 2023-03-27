03bf12d5c46735374c4d2e35d4d2769186da22eb
Atte Liimatainen <atte.liimat@gmail.com>
2023-03-27 21:24:05 +0300

    add custom max-height, so hero doesnt stretch too much

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
         "16/9": "16 / 9",
         "9/16": "9 / 16",
       },
+      maxHeight: {
+        "67.5rem": "67.5rem",
+      },
     },
   },
   plugins: [require("@tailwindcss/typography")],
