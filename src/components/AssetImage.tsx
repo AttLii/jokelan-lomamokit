284c48ba531f0c32f64fb2364ed621f4df570996
Atte Liimatainen <atte.liimat@gmail.com>
2023-03-27 20:11:32 +0300

    add hero section

---
+++ src/components/AssetImage.tsx
@@ -0,0 +1,18 @@
+import { component$ } from "@builder.io/qwik";
+import type { ParsedImageAsset } from "~/parsers/contentful"
+
+type Props = ParsedImageAsset & {
+  _class?: string,
+  loading: "eager" | "lazy",
+  srcSet?: Record<string, string>
+}
+export const AssetImage = component$(({ _class, alt, loading, height, src, width, srcSet }: Props) => {
+  return (
+    <picture>
+      {srcSet && Object.keys(srcSet)?.map((key, i) => (
+        <source key={i} media={key} srcSet={srcSet[key]} />
+      ))}
+      <img class={_class} src={src} loading={loading} alt={alt} height={height} width={width} decoding="async" />
+    </picture>
+  )
+})
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-    extend: {},
+    extend: {
+      aspectRatio: {
+        "16/9": "16 / 9",
+        "9/16": "9 / 16",
+      },
+    },
   },
   plugins: [],
 };
