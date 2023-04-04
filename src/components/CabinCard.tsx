8155938e724b189076af54d80f084c4b1e677057
Atte Liimatainen <atte.liimat@gmail.com>
2023-04-04 23:08:42 +0300

    add link around card image

--- src/components/CabinCard.tsx
+++ src/components/CabinCard.tsx
@@ -3,6 +3,7 @@ import type { ParsedCabinReference } from "~/parsers/contentful";
 import { ButtonLink } from "./ButtonLink";
 import { translations } from "~/constants/translations";
 import { AssetImage } from "./AssetImage";
+import { Link } from "@builder.io/qwik-city";
 
 type Props = {
   cabinReference: ParsedCabinReference
@@ -10,8 +11,10 @@ type Props = {
 export const CabinCard = component$(({ cabinReference: { path, title, image: { src, ...imageRest } } }: Props) => {
   return (
     <article class="bg-slate-400 rounded-xl overflow-hidden">
-      <AssetImage {...imageRest} loading="lazy"
-        src={`${src}?w=608&h=342`} />
+      <Link href={path} aria-label={translations.genericReadMore}>
+        <AssetImage {...imageRest} loading="lazy"
+          src={`${src}?w=608&h=342`} />
+      </Link>
       <div class="p-2">
         <h3 class="mb-2">{title}</h3>
         <ButtonLink href={path}>{translations.genericReadMore}</ButtonLink>
