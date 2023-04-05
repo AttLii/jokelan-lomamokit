6fa8662348807bddefe30f8cc0673cd7b2f095e6
Atte Liimatainen <atte.liimat@gmail.com>
2023-04-05 14:49:37 +0300

    change to post

--- src/routes/api/contact-form/index.tsx
+++ src/routes/api/contact-form/index.tsx
@@ -1,6 +1,6 @@
 import type { RequestHandler } from '@builder.io/qwik-city';
 
 // Called with every HTTP request (regardless of method)
-export const onRequest: RequestHandler = async ({ json }) => {
+export const onPost: RequestHandler = async ({ json }) => {
   json(200, { hello: 'world' });
 }
