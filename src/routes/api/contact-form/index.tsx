b27113daeb15540fdc3b2bf7e187f67f71593ed4
Atte Liimatainen <atte.liimat@gmail.com>
2023-04-05 14:56:02 +0300

    try vercel serverless function

--- src/routes/api/contact-form/index.tsx
+++
@@ -1,6 +0,0 @@
-import type { RequestHandler } from '@builder.io/qwik-city';
-
-// Called with every HTTP request (regardless of method)
-export const onPost: RequestHandler = async ({ json }) => {
-  json(200, { hello: 'world' });
-}
