cdf4614a6245d5dcf8b1e19be5b1d249d931ac67
Atte Liimatainen <atte.liimat@gmail.com>
2023-04-10 11:49:28 +0300

    add fit=fill to places where its needed

--- src/components/CabinCard.tsx
+++ src/components/CabinCard.tsx
@@ -13,7 +13,7 @@ export const CabinCard = component$(({ cabinReference: { path, title, image: { s
     <article class="bg-slate-200 border-black border-2 rounded-md overflow-hidden">
       <Link href={path} aria-label={translations.genericReadMore}>
         <AssetImage {...imageRest} loading="lazy"
-          src={`${src}&w=608&h=342`} />
+          src={`${src}&w=608&h=342&fit=fill`} />
       </Link>
       <div class="p-2">
         <h3 class="mb-2">{title}</h3>
