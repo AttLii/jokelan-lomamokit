b90640dd91a6ff7dc596f26fde87391fa2ce3a44
Atte Liimatainen <atte.liimat@gmail.com>
2023-04-12 18:07:47 +0300

    fix issues related to header height

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -12,8 +12,8 @@ export const MobileMenu = component$(({ menu }: Props) => {
   const menuLeft = ui.nav ? "right-0" : "-right-full"
   return (
     <>
-      <div onClick$={() => ui.nav = !ui.nav} class={`cursor-pointer fixed bottom-0 h-[calc(100vh-2.5rem)] w-screen bg-black transition-opacity duration-500 ${backdrop}`} />
-      <nav class={`block sm:hidden border-l-2 border-black fixed bottom-0 w-full max-w-xs h-[calc(100vh-2.5rem)] bg-slate-400 mx-auto px-2 py-4 transition-all duration-500 ${menuLeft}`}
+      <div onClick$={() => ui.nav = !ui.nav} class={`cursor-pointer fixed bottom-0 h-[calc(100vh-3.5rem)] w-screen bg-black transition-opacity duration-500 ${backdrop}`} />
+      <nav class={`block sm:hidden border-l-2 border-black fixed bottom-0 w-full max-w-xs h-[calc(100vh-3.5rem)] bg-slate-400 mx-auto px-2 py-4 transition-all duration-500 ${menuLeft}`}
         aria-label={menu.title}>
         <ul class="flex flex-col gap-x-4">
           {menu.menuItems.map((menuItem, i) => (
