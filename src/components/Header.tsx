d1375a8703c3095b4a71f1291e6bca168d172579
Atte Liimatainen <atte.liimat@gmail.com>
2023-04-12 19:55:49 +0300

    separate logo link to it's component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,9 +3,8 @@ import { BurgerButton } from "./BurgerButton";
 import { DesktopMenu } from "./DesktopMenu";
 import { MobileMenu } from "./MobileMenu";
 import { Container } from "./Container";
+import { LogoLink } from "./LogoLink";
 import type { ParsedMenu } from "~/parsers/contentful";
-import { Link } from "@builder.io/qwik-city";
-import { translations } from "~/constants/translations";
 
 type Props = {
   menu: ParsedMenu
@@ -14,9 +13,7 @@ export const Header = component$(({ menu }: Props) => {
   return (
     <header class="fixed w-full bg-slate-100 z-10 h-14 border-black border-b-2 py-2">
       <Container type="wide" _class="flex h-full align-middle justify-between">
-        <Link href="/" aria-label={translations.genericLogoLink}>
-          <img src="/logo.svg" loading="eager" width="40" height="40" alt="" />
-        </Link>
+        <LogoLink />
         <DesktopMenu menu={menu} />
         <div />
         <BurgerButton _class="block sm:hidden ml-auto my-auto" />
