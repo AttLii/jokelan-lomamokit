b545f7d2aebf351372c261e97f540cfacf6f1202
Atte Liimatainen <atte.liimat@gmail.com>
2023-04-12 19:46:35 +0300

    double side padding on container

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -7,5 +7,5 @@ export type Props = {
 
 export const Container = component$(({ type, _class = "" }: Props) => {
   const maxWidth = type === "wide" ? "max-w-7xl" : "max-w-xl"
-  return <div class={`mx-auto px-2 ${maxWidth} ${_class}`}><Slot /></div>
+  return <div class={`mx-auto px-4 ${maxWidth} ${_class}`}><Slot /></div>
 })
--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -8,7 +8,7 @@ export const HeroSection = component$(({ gallery, richText }: Props) => {
   return (
     <section class="relative overflow-hidden aspect-[9/16] sm:aspect-[16/9] z-0 max-h-[50rem] w-full border-black border-b-2">
       <Carousel images={gallery} />
-      <div class="flex items-center justify-center p-6 absolute top-0 left-0 right-0 bottom-0 bg-black bg-opacity-10">
+      <div class="flex items-center justify-center p-4 absolute top-0 left-0 right-0 bottom-0 bg-black bg-opacity-10">
         <RichText
           _class="h-max w-full max-w-xl text-white"
           dangerouslySetInnerHTML={richText}
