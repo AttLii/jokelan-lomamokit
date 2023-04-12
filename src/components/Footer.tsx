fe2eefe59a95bb0950591a4877ec3402d697b3ec
Atte Liimatainen <atte.liimat@gmail.com>
2023-04-12 20:31:50 +0300

    make navigation horizontal on desktop

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,18 +10,20 @@ type Props = {
 }
 export const Footer = component$(({ menu: { title, menuItems }, contactInformation }: Props) => {
   return (
-    <footer class="w-full bg-slate-100 py-4">
+    <footer class="w-full bg-slate-100 py-4 border-2 border-t-2 border-black">
       <Container type="wide" _class="flex flex-col gap-4">
-        <nav aria-label={title}>
-          <ul class="flex flex-col gap-0.5">
-            {menuItems.map((menuItem, i) => (
-              <li key={i}>
-                <MenuLink menuItem={menuItem} />
-              </li>
-            ))}
-          </ul>
-        </nav>
-        <RichText dangerouslySetInnerHTML={contactInformation} />
+        <div class="flex flex-col gap-4">
+          <nav aria-label={title}>
+            <ul class="flex flex-col md:flex-row gap-x-4 gap-y-1">
+              {menuItems.map((menuItem, i) => (
+                <li key={i}>
+                  <MenuLink menuItem={menuItem} />
+                </li>
+              ))}
+            </ul>
+          </nav>
+          <RichText dangerouslySetInnerHTML={contactInformation} _class="w-full md:w-1/2" />
+        </div>
         <hr />
         <p class="text-center">© {(new Date()).getFullYear()} Jokelan Lomamökit</p>
       </Container>
