0ff24417ebb46ef700ee40f89a3f8d758fc91e91
Atte Liimatainen <atte.liimat@gmail.com>
2023-04-14 20:46:10 +0300

    move submenu renderer to parent component

--- src/components/DesktopMenu.tsx
+++ src/components/DesktopMenu.tsx
@@ -1,6 +1,26 @@
 import { component$ } from "@builder.io/qwik";
-import type { ParsedMenu } from "~/parsers/contentful";
+import type { ParsedMenu, ParsedMenuItem } from "~/parsers/contentful";
 import { MenuLink } from "./MenuLink";
+
+type SubMenuRendererProps = {
+  subItems: ParsedMenuItem[]
+}
+const SubMenuRenderer = component$(({ subItems }: SubMenuRendererProps) => (
+  <div class="
+      w-full min-w-[7.5rem] absolute top-full left-1/2 -translate-x-1/2 pt-[0.95rem]
+      opacity-0
+      group-focus-within:opacity-100 group-hover:opacity-100
+      transition-opacity
+    ">
+    <ul class="flex flex-col gap-2 bg-slate-100 border-black border-2 border-t-0 p-4">
+      {subItems.map((subItem, i) => (
+        <li key={i}>
+          <MenuLink menuItem={subItem} showSubItems={false} />
+        </li>
+      ))}
+    </ul>
+  </div>
+))
 
 type Props = {
   _class: string;
@@ -12,7 +32,12 @@ export const DesktopMenu = component$(({ _class, menu: { title, menuItems } }: P
       <ul class="flex flex-wrap gap-x-4 my-auto">
         {menuItems.map((menuItem, i) => (
           <li key={i}>
-            <MenuLink menuItem={menuItem} showSubItems _class="min-w-[5rem] text-center" />
+            <MenuLink
+              menuItem={menuItem}
+              showSubItems
+              SubMenuRenderer={SubMenuRenderer}
+              _class="min-w-[5rem] text-center"
+            />
           </li>
         ))}
       </ul>
--- src/components/MenuLink.tsx
+++ src/components/MenuLink.tsx
@@ -1,14 +1,21 @@
+import type { Component } from "@builder.io/qwik";
 import { component$ } from "@builder.io/qwik";
 import { Link, useLocation } from "@builder.io/qwik-city";
 import { HiChevronDown } from "@qwikest/icons/heroicons";
 import type { ParsedMenuItem } from "~/parsers/contentful";
 import { areRelativePathsSame } from "~/utils/qwik";
 
+export type SubItemsRendererProps = {
+  subItems: ParsedMenuItem[]
+}
+
 type Props = {
   menuItem: ParsedMenuItem,
   _class?: string,
   showSubItems: boolean;
+  SubMenuRenderer?: Component<SubItemsRendererProps>
 }
+
 export const MenuLink = component$(({
   menuItem: {
     title,
@@ -18,7 +25,8 @@ export const MenuLink = component$(({
     subItems
   },
   _class = "",
-  showSubItems
+  showSubItems,
+  SubMenuRenderer
 }: Props) => {
   const location = useLocation()
   const activeClass = areRelativePathsSame(location.url.pathname, path)
@@ -35,21 +43,8 @@ export const MenuLink = component$(({
       {showIcon && (
         <HiChevronDown variant="mini" class="absolute right-0 h-6 w-6 rotate-0 group-focus-within:rotate-180 group-hover:rotate-180 transition-transform" />
       )}
-      {showIcon && (
-        <div class="
-          w-full min-w-[7.5rem] absolute top-full left-1/2 -translate-x-1/2 pt-[0.95rem]
-          opacity-0
-          group-focus-within:opacity-100 group-hover:opacity-100
-          transition-opacity
-        ">
-          <ul class="flex flex-col gap-2 bg-slate-100 border-black border-2 border-t-0 p-4">
-            {subItems.map((subItem, i) => (
-              <li key={i}>
-                <MenuLink menuItem={subItem} showSubItems={false} />
-              </li>
-            ))}
-          </ul>
-        </div>
+      {showIcon && SubMenuRenderer && (
+        <SubMenuRenderer subItems={subItems} />
       )}
     </div>
   )
