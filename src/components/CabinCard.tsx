dddaf2fb8d0afb5da5f6226f6736725403168426
Atte Liimatainen <atte.liimat@gmail.com>
2023-04-14 19:02:37 +0300

    fix accessibility warning related to cabin card

--- src/components/CabinCard.tsx
+++ src/components/CabinCard.tsx
@@ -3,7 +3,6 @@ import type { ParsedCabinReference } from "~/parsers/contentful";
 import { IconLink } from "./IconLink";
 import { translations } from "~/constants/translations";
 import { AssetImage } from "./AssetImage";
-import { Link } from "@builder.io/qwik-city";
 
 type Props = {
   cabinReference: ParsedCabinReference
@@ -11,15 +10,13 @@ type Props = {
 export const CabinCard = component$(({ cabinReference: { path, title, image: { src, ...imageRest } } }: Props) => {
   return (
     <article class="bg-slate-100 border-black border-2 rounded-md overflow-hidden">
-      <Link href={path} aria-label={translations.genericReadMore}>
-        <AssetImage
-          {...imageRest}
-          width="608"
-          height="456"
-          loading="lazy"
-          src={`${src}&w=608&h=456&fit=fill`}
-        />
-      </Link>
+      <AssetImage
+        {...imageRest}
+        width="608"
+        height="456"
+        loading="lazy"
+        src={`${src}&w=608&h=456&fit=fill`}
+      />
       <div class="p-4 border-black border-t-2">
         <h3 class="text-2xl font-bold mb-2">{title}</h3>
         <IconLink href={path}>{translations.genericReadMore}</IconLink>
