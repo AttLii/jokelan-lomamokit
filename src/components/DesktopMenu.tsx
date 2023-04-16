c077c38c4f62eff29e7bf73bb7b7dd3b0087d400
Atte Liimatainen <atte.liimat@gmail.com>
2023-04-17 00:00:09 +0300

    fix bug related to submenu opening out of nowhere

--- src/components/DesktopMenu.tsx
+++ src/components/DesktopMenu.tsx
@@ -6,8 +6,8 @@ import type { SubItemsRendererProps } from "./MenuLink";
 const SubMenuRenderer = component$(({ subItems }: SubItemsRendererProps) => (
   <div class="
       w-full min-w-[7.5rem] absolute top-full left-1/2 -translate-x-1/2 pt-[0.95rem]
-      opacity-0
-      group-focus-within:opacity-100 group-hover:opacity-100
+      opacity-0 pointer-events-none
+      group-focus-within:opacity-100 group-hover:opacity-100 group-focus-within:pointer-events-auto group-hover:pointer-events-auto
       transition-opacity
     ">
     <ul class="flex flex-col gap-2 bg-slate-100 border-black border-2 border-t-0 p-4">
