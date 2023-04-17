19a3a21ac85123a7ab2340d5b52cec9a6b79f1e7
Atte Liimatainen <atte.liimat@gmail.com>
2023-04-17 14:51:32 +0300

    add scroll functionality to mini images

--- src/components/CabinGallery.tsx
+++ src/components/CabinGallery.tsx
@@ -1,4 +1,4 @@
-import { $, component$, useSignal } from "@builder.io/qwik";
+import { $, component$, useSignal, useVisibleTask$ } from "@builder.io/qwik";
 import type { ParsedImageAsset } from "~/parsers/contentful";
 import { AssetImage } from "./AssetImage";
 import { translations } from "~/constants/translations";
@@ -7,13 +7,24 @@ type Props = {
   gallery: ParsedImageAsset[]
 }
 export const CabinGallery = component$(({ gallery }: Props) => {
+  const scrollContainer = useSignal<Element>()
   const index = useSignal(0)
   const onClick = $((i: number) => index.value = i)
+
+  useVisibleTask$(({ track }) => {
+    track(index)
+    track(scrollContainer)
+
+    if (!scrollContainer.value || gallery.length === 0) return
+
+    const child = scrollContainer.value.children[index.value] as HTMLElement
+    if (!child) return
+    child.scrollIntoView({ behavior: "smooth", inline: "center", block: "end" });
+  })
   return (
     <div class="flex flex-col gap-2">
       <div>
         <div class="border-2 border-black rounded-md overflow-hidden">
-
           <ul class="flex flex-nowrap transition-transform duration-1000" style={{
             transform: `translateX(${index.value === 0 ? 0 : index.value * 100 * -1}%)`
           }}>
@@ -24,14 +35,16 @@ export const CabinGallery = component$(({ gallery }: Props) => {
             ))}
           </ul>
         </div>
-        <p class="mt-1 text-center">{gallery[index.value].alt}</p>
+        {gallery[index.value] && (
+          <p class="mt-1 text-center">{gallery[index.value].alt}</p>
+        )}
       </div>
 
-      <ul class="flex flex-nowrap gap-4 overflow-x-scroll snap-x pb-2">
+      <ul ref={scrollContainer} class="flex flex-nowrap overflow-x-scroll snap-x pb-2 -mx-2">
         {gallery.map(({ src }, i) => (
-          <li key={i} class="min-w-[17.5%] snap-start">
+          <li key={i} class="min-w-[20%] snap-start px-2">
             <button onclick$={() => onClick(i)} class={`${index.value === i ? "rounded-md overflow-hidden outline-double outline-2 outline-black  -outline-offset-2" : ""}`}>
-              <AssetImage alt={`${translations.cabinGalleryGoTo} ${i + 1}`} loading={i === 0 ? "eager" : "lazy"} width="162" height="91" src={`${src}&w=162&h=91`} />
+              <AssetImage alt={`${translations.cabinGalleryGoTo} ${i + 1}`} loading={i === 0 ? "eager" : "lazy"} width="174" height="100" src={`${src}&w=174&h=100`} />
             </button>
           </li>
         ))}
