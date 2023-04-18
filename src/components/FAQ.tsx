6e2e31dfa1ee50b47550215aa9256a98e3c10407
Atte Liimatainen <atte.liimat@gmail.com>
2023-04-18 10:41:53 +0300

    make click area bigger

--- src/components/FAQ.tsx
+++ src/components/FAQ.tsx
@@ -7,9 +7,9 @@ type Props = {
 }
 export const FAQ = component$(({ faq: { answer, question } }: Props) => {
   return (
-    <details class="bg-slate-100 border-black border-2 rounded-md overflow-hidden p-4">
-      <summary class="cursor-pointer font-display font-bold">{question}</summary>
-      <RichText _class="mt-4" dangerouslySetInnerHTML={answer} />
+    <details class="bg-slate-100 border-black border-2 rounded-md overflow-hidden">
+      <summary class="p-4 cursor-pointer font-display font-bold">{question}</summary>
+      <RichText _class="p-4 pt-0" dangerouslySetInnerHTML={answer} />
     </details>
   )
 })
