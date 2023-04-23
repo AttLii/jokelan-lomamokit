b6c4a0e3ede9a298664fd9b0a2835b21c3cd97bc
Atte Liimatainen <atte.liimat@gmail.com>
2023-04-23 20:48:28 +0300

    remove faq page structured data
    - will be moved to server side

--- src/components/FAQsSection.tsx
+++ src/components/FAQsSection.tsx
@@ -1,30 +1,11 @@
 import { component$ } from "@builder.io/qwik";
 import { SectionWithRichText } from "./SectionWithRichText";
 import { FAQ } from "./FAQ";
-import type { ParsedFAQ, ParsedFAQsSection } from "~/parsers/contentful";
+import type { ParsedFAQsSection } from "~/parsers/contentful";
 
-const composeJSONLDfromParsedFaqs = (faqs: ParsedFAQ[]) => {
-  const obj = {
-    "@context": "https://schema.org",
-    "@type": "FAQPage",
-    "mainEntity": faqs.map(({ answer, question }) => {
-      return {
-        "@type": "Question",
-        "name": question,
-        "acceptedAnswer": {
-          "@type": "Answer",
-          "text": answer
-        }
-      }
-    })
-  }
-
-  return obj
-}
 
 type Props = ParsedFAQsSection
 export const FAQsSection = component$(({ richText, faqs }: Props) => {
-  const faqPageJsonLD = composeJSONLDfromParsedFaqs(faqs)
   return (
     <SectionWithRichText type="narrow" richText={richText}>
       <ul class="flex flex-col gap-2">
@@ -34,7 +15,6 @@ export const FAQsSection = component$(({ richText, faqs }: Props) => {
           </li>
         ))}
       </ul>
-      <script type="application/ld+json" dangerouslySetInnerHTML={JSON.stringify(faqPageJsonLD)} />
     </SectionWithRichText>
   )
 })
