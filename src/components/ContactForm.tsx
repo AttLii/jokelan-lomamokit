027e1a4633c27564f6ac044d7aec4910d59356e2
Atte Liimatainen <atte.liimat@gmail.com>
2023-04-24 14:24:20 +0300

    fix warning in ContactForm component
    - define messages in root

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -7,15 +7,19 @@ import { t } from "~/stores/translation";
 import type { QwikSubmitEvent } from "@builder.io/qwik";
 
 export const ContactForm = component$(() => {
+  const successMessage = t('contact.form.submit.success')
+  const serverErrorMessage = t('contact.form.submit.server.error')
+  const clientErrorMessage = t('contact.form.submit.success')
+
   const submitting = useSignal(false)
   const message = useSignal("")
-  const onSubmit = $(async (_: QwikSubmitEvent<HTMLFormElement>, form: HTMLFormElement) => {
+  const useSubmit = $(async (_: QwikSubmitEvent<HTMLFormElement>, form: HTMLFormElement) => {
     submitting.value = true;
     message.value = ""
-
     const formData = new FormData(form)
 
-    const { status } = await fetch((import.meta.env.VITE_ENDPOINT_ORIGIN || "") + "/api/contact-form", {
+    const url = (import.meta.env.VITE_ENDPOINT_ORIGIN || "") + "/api/contact-form"
+    const { status } = await fetch(url, {
       body: JSON.stringify(Object.fromEntries(formData)),
       method: "post",
       headers: {
@@ -23,20 +27,20 @@ export const ContactForm = component$(() => {
         'Content-Type': 'application/json'
       },
     })
-
     if (status === 201) {
-      message.value = t('contact.form.submit.success')
+      message.value = successMessage
       form.reset()
     } else if (status === 404 || status >= 500 && status < 600) {
-      message.value = t('contact.form.submit.server.error')
+      message.value = serverErrorMessage
     } else if (status >= 400 && status < 500) {
-      message.value = t('contact.form.submit.client.error')
+      message.value = clientErrorMessage
     }
 
     submitting.value = false
   })
+
   return (
-    <form preventdefault:submit onSubmit$={onSubmit} class="flex flex-col gap-4 p-4 bg-slate-100 border-black border-2 rounded-md">
+    <form preventdefault:submit onSubmit$={useSubmit} class="flex flex-col gap-4 p-4 bg-slate-100 border-black border-2 rounded-md">
       <Input type="text" name="name" required label={t('contact.form.name')} disabled={submitting.value} />
       <Input type="email" name="email" required label={t('contact.form.email')} disabled={submitting.value} />
       <Input type="tel" name="tel" label={t('contact.form.phonenumber')} disabled={submitting.value} />
