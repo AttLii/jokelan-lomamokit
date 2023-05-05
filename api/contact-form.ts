c086abae48a47844d5248174836bc20a8f18dc35
Atte Liimatainen <atte.liimat@gmail.com>
2023-05-05 16:48:12 +0300

    improve contact-form endpoint
    - create schema at the start of the function
    -  remove processResponse for being cute

--- api/contact-form.ts
+++ api/contact-form.ts
@@ -3,15 +3,6 @@ import { z } from "zod";
 import { verify } from "hcaptcha";
 import { GoogleSpreadsheet } from "google-spreadsheet";
 
-const formSchema = z.object({
-  name: z.string(),
-  email: z.string().email(),
-  tel: z.string(),
-  message: z.string(),
-  "h-captcha-response": z.string().min(1),
-  date: z.string().optional(),
-});
-
 const allowCors =
   (fn: (req: VercelRequest, res: VercelResponse) => void) =>
   async (req: VercelRequest, res: VercelResponse) => {
@@ -37,28 +28,41 @@ const handler = async (
   { method, body }: VercelRequest,
   response: VercelResponse
 ) => {
-  const processResponse = (status: number, message: string) =>
-    response.status(status).json({ message });
-
   if (method !== "POST") {
-    return processResponse(404, "Unsupported method");
+    return response.status(404).json({ message: "Unsupported method" });
   }
 
-  const validatedBody = formSchema.safeParse(body);
-  if (!validatedBody.success) {
-    return processResponse(422, "Provided body is in unexpected form");
+  const result = z
+    .object({
+      name: z.string(),
+      email: z.string().email(),
+      tel: z.string(),
+      message: z.string(),
+      "h-captcha-response": z.string(),
+    })
+    .safeParse(body);
+
+  if (!result.success) {
+    return response
+      .status(422)
+      .json({ message: "Provided body is in unexpected form" });
   }
 
-  const { "h-captcha-response": token, ...bodyWithoutHCaptcha } =
-    validatedBody.data;
-
-  const hCaptchaVerification = await verify(
-    process.env.VITE_HCAPTCHA_SECRET + "",
-    token
-  );
-
-  if (!hCaptchaVerification.success) {
-    return processResponse(422, "Provided body is in unexpected form");
+  const { "h-captcha-response": token, ...dataWithoutToken } = result.data;
+  try {
+    const tokenResult = await verify(
+      process.env.VITE_HCAPTCHA_SECRET + "",
+      token
+    );
+    if (!tokenResult.success) {
+      return response
+        .status(422)
+        .json({ message: "Couldn't verify h-captcha response" });
+    }
+  } catch {
+    return response
+      .status(500)
+      .json({ message: "Couldn't verify h-captcha-response" });
   }
 
   try {
@@ -69,16 +73,17 @@ const handler = async (
     });
     await doc.loadInfo();
     const sheet = doc.sheetsByIndex[0];
-    bodyWithoutHCaptcha["date"] = new Date().toISOString();
-    if (!sheet.headerValues) {
-      await sheet.setHeaderRow(Object.keys(bodyWithoutHCaptcha));
-    }
-    await sheet.addRow(bodyWithoutHCaptcha);
+    await sheet.addRow({
+      ...dataWithoutToken,
+      date: new Date().toISOString(),
+    });
   } catch {
-    return processResponse(500, "Couldn't save the form data");
+    return response
+      .status(500)
+      .json({ message: "Couldn't save the form data" });
   }
 
-  return processResponse(201, "Form saved successfully");
+  return response.status(201).json({ message: "Form saved successfully" });
 };
 
 export default process.env.NODE_ENV === "production"
