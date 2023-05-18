a59d8b05870c040fba9dfea7a46da6b24a6bf413
Atte Liimatainen <atte.liimat@gmail.com>
2023-05-18 22:53:24 +0300

    add assets

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,10 +4,14 @@ import { StringTranslationContext } from '../contexts/stringTranslations.ts'
 import { GlobalContentContext } from '../contexts/globalContent.ts'
 import stringTranslations from '../prevals/stringTranslations.preval.ts'
 import globalContent from '../prevals/globalContent.preval.ts'
+import Head from 'next/head'
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
       <GlobalContentContext.Provider value={globalContent}>
         <StringTranslationContext.Provider value={stringTranslations}>
           <Component {...pageProps} />
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 const Document = () => {
   return (
     <Html lang="fi">
-      <Head />
+      <Head>
+        <meta charSet="utf-8" />
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+      </Head>
       <body>
         <Main />
         <NextScript />
