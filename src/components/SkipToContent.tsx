248d2dcf9206a55047d0f0b3c8248bd12cc40a81
Atte Liimatainen <atte.liimat@gmail.com>
2023-05-19 14:51:44 +0300

    add skip to content

---
+++ src/components/SkipToContent.tsx
@@ -0,0 +1,26 @@
+import { FC, MutableRefObject } from "react"
+import { useT } from "../contexts/stringTranslations"
+
+type Props = {
+  focusElement: MutableRefObject<HTMLElement | null>
+}
+export const SkipToContent: FC<Props> = ({ focusElement }) => {
+  const label = useT('generic.skip.to.content')
+  const onClick = () => {
+    if (!focusElement.current) return
+    console.log('hi')
+    focusElement.current.focus()
+  }
+  return (
+    <button
+      className="
+        z-50 left-1/2 fixed -translate-x-1/2 -translate-y-full focus:translate-y-0 -top-1 focus:top-1
+        transition duration-0
+        font-sans color-black bg-slate-300 rounded-md p-2 border-black border-2
+      "
+      onClick={onClick}
+    >
+      {label}
+    </button>
+  )
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,14 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
 import { StringTranslationContext } from '../contexts/stringTranslations.ts'
-import { GlobalContentContext } from '../contexts/globalContent.ts'
-import stringTranslations from '../prevals/stringTranslations.preval.ts'
-import globalContent from '../prevals/globalContent.preval.ts'
+import { useRef } from 'react'
 import Head from 'next/head'
 import { Cabin, PT_Sans } from "next/font/google"
 import { Footer } from '../components/Footer.tsx'
+import { SkipToContent } from '../components/SkipToContent.tsx'
+import { GlobalContentContext } from '../contexts/globalContent.ts'
+import stringTranslations from '../prevals/stringTranslations.preval.ts'
+import globalContent from '../prevals/globalContent.preval.ts'
 
 const cabinFont = Cabin({
   subsets: ["latin"],
@@ -19,6 +21,7 @@ const ptSansFont = PT_Sans({
 })
 
 const App = ({ Component, pageProps }: AppProps) => {
+  const main = useRef<HTMLElement | null>(null)
   return (
     <div className={`${cabinFont.variable} ${ptSansFont.variable}`}>
       <Head>
@@ -27,7 +30,8 @@ const App = ({ Component, pageProps }: AppProps) => {
       </Head>
       <GlobalContentContext.Provider value={globalContent}>
         <StringTranslationContext.Provider value={stringTranslations}>
-          <main>
+          <SkipToContent focusElement={main} />
+          <main ref={main}>
             <Component {...pageProps} />
           </main>
           <Footer />
