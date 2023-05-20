c6c1bbd9b0e3e3045290375111108388318640ab
Atte Liimatainen <atte.liimat@gmail.com>
2023-05-20 11:40:35 +0300

    add dynamic imports for page sections

--- src/components/SectionsRenderer.tsx
+++ src/components/SectionsRenderer.tsx
@@ -1,14 +1,16 @@
-import type { FC} from "react";
+import type { FC } from "react";
 import { Fragment } from "react";
+import dynamic from "next/dynamic";
 import type { ParsedSections } from "../parsers/contentful";
 import { isParsedCabinReferences, isParsedContent, isParsedFaqs, isParsedFiftyFifty, isParsedForm, isParsedHero, isParsedMap } from "../typeguards/contentful";
-import { Hero } from "./sections/Hero";
-import { FiftyFifty } from "./sections/FiftyFifty";
-import { Map } from "./sections/Map";
-import { CabinReferencesSection } from "./sections/CabinReferences";
-import { Form } from "./sections/Form";
-import { Content } from "./sections/Content";
-import { FAQs } from "./sections/Faqs";
+
+const Hero = dynamic(() => import("./sections/Hero").then((mod) => mod.Hero));
+const FiftyFifty = dynamic(() => import("./sections/FiftyFifty").then((mod) => mod.FiftyFifty));
+const Map = dynamic(() => import("./sections/Map").then((mod) => mod.Map));
+const CabinReferences = dynamic(() => import("./sections/CabinReferences").then((mod) => mod.CabinReferencesSection));
+const Form = dynamic(() => import("./sections/Form").then((mod) => mod.Form));
+const Content = dynamic(() => import("./sections/Content").then((mod) => mod.Content));
+const Faqs = dynamic(() => import("./sections/Faqs").then((mod) => mod.FAQs));
 
 type Props = {
   sections: ParsedSections
@@ -24,13 +26,13 @@ export const SectionsRenderer: FC<Props> = ({ sections }) => {
         } else if (isParsedMap(section)) {
           return <Map key={i} section={section} />;
         } else if (isParsedCabinReferences(section)) {
-          return <CabinReferencesSection key={i} section={section} />;
+          return <CabinReferences key={i} section={section} />;
         } else if (isParsedForm(section)) {
           return <Form key={i} section={section} />;
         } else if (isParsedContent(section)) {
           return <Content key={i} section={section} />;
         } else if (isParsedFaqs(section)) {
-          return <FAQs key={i} section={section} />;
+          return <Faqs key={i} section={section} />;
         } else {
           return <Fragment key={i} />;
         }
