a0b759228fbdcbaa938565a64916a7dc282ef88f
Atte Liimatainen <atte.liimat@gmail.com>
2023-05-22 14:19:51 +0300

    type srcset keys

--- src/components/AssetImage.tsx
+++ src/components/AssetImage.tsx
@@ -6,9 +6,14 @@ type ContentfulImageProps = {
   width: string | number;
   height: string | number;
 }
+
+type Breakpoint = 640 | 768 | 1024;
+type MediaQuery = "min-width";
+type SrcSetKey = `(${MediaQuery}: ${Breakpoint}px)`
+
 type Props = Omit<ImgProps, "srcSet"> & ContentfulImageProps & {
   src: string;
-  srcSet?: Record<string, Required<ContentfulImageProps>>
+  srcSet?: Record<SrcSetKey, Required<ContentfulImageProps>>
 }
 
 const AssetImage: FC<Props> = ({ alt = "", loading = "lazy", src, fit, width, height, srcSet, ...props }) => {
@@ -23,9 +28,16 @@ const AssetImage: FC<Props> = ({ alt = "", loading = "lazy", src, fit, width, he
   />;
   return srcSet ?
     <picture>
-      {Object.keys(srcSet).map((key) => (
-        <source key={key} media={key} srcSet={`${src}&fit=${srcSet[key].fit}&w=${srcSet[key].width}&h=${srcSet[key].height}`} />
-      ))}
+      {Object.keys(srcSet).map((_key) => {
+        const key = _key as SrcSetKey;
+        return (
+          <source
+            key={key}
+            media={key}
+            srcSet={`${src}&fit=${srcSet[key].fit}&w=${srcSet[key].width}&h=${srcSet[key].height}`}
+          />
+        );
+      })}
       {Image}
     </picture>
     : Image;
