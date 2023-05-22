5c5410bda51666f44a0f7716889acc4dfd67824b
Atte Liimatainen <atte.liimat@gmail.com>
2023-05-22 15:10:52 +0300

    remove hcaptcha related code
    - isnt gdpr compliant

--- env.d.ts
+++ env.d.ts
@@ -4,8 +4,6 @@ namespace NodeJS {
     CONTENTFUL_ACCESS_TOKEN: string;
     NEXT_PUBLIC_ORIGIN: string;
     NEXT_PUBLIC_ENDPOINT_ORIGIN: string;
-    NEXT_PUBLIC_HCAPTCHA_SITE_KEY: string;
-    HCAPTCHA_SECRET: string;
     GOOGLE_SERVICE_ACCOUNT_EMAIL: string;
     GOOGLE_PRIVATE_KEY: string;
     GOOGLE_SHEETS_ID: string;
