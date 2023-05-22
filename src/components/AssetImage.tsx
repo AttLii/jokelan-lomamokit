57c1391d6bd76fb06c9fa48562751fc14c9d45a6
Atte Liimatainen <atte.liimat@gmail.com>
2023-05-22 14:05:50 +0300

    override srcSet prop

--- src/components/AssetImage.tsx
+++ src/components/AssetImage.tsx
@@ -1,29 +1,34 @@
 import type { DetailedHTMLProps, FC, ImgHTMLAttributes } from "react";
 
 type ImgProps = DetailedHTMLProps<ImgHTMLAttributes<HTMLImageElement>, HTMLImageElement>
-
-type Props = ImgProps & {
+type ContentfulImageProps = {
   fit?: "pad" | "fill";
   width: string | number;
   height: string | number;
+}
+type Props = Omit<ImgProps, "srcSet"> & ContentfulImageProps & {
   src: string;
+  srcSet?: Record<string, Required<ContentfulImageProps>>
 }
 
-const AssetImage: FC<Props> = ({ alt = "", loading = "lazy", src, fit, width, height, ...props }) => {
-  if (fit) {
-    src += `&fit=${fit}&w=${width}&h=${height}`;
-  }
-  return (
-    <img
-      decoding="async"
-      alt={alt}
-      loading={loading}
-      src={src}
-      width={width}
-      height={height}
-      {...props}
-    />
-  );
+const AssetImage: FC<Props> = ({ alt = "", loading = "lazy", src, fit, width, height, srcSet, ...props }) => {
+  const Image = <img
+    decoding="async"
+    alt={alt}
+    loading={loading}
+    src={fit ? `${src}&fit=${fit}&w=${width}&h=${height}` : src}
+    width={width}
+    height={height}
+    {...props}
+  />;
+  return srcSet ?
+    <picture>
+      {Object.keys(srcSet).map((key) => (
+        <source key={key} media={key} srcSet={`${src}&fit=${srcSet[key].fit}&w=${srcSet[key].width}&h=${srcSet[key].height}`} />
+      ))}
+      {Image}
+    </picture>
+    : Image;
 };
 
 export default AssetImage;
