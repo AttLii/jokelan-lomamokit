613d1455bfa5131d58c36b91ab27d5443ba1307a
Atte Liimatainen <atte.liimat@gmail.com>
2023-05-25 20:43:54 +0300

    create contentful client using factories

---
+++ src/factories/client.ts
@@ -0,0 +1,11 @@
+import * as contentful from "contentful";
+import Contentful from "../repositories/contentful";
+
+const client = new Contentful(
+  contentful.createClient({
+    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+    space: process.env.CONTENTFUL_SPACE,
+  }).withoutUnresolvableLinks
+);
+
+export default client;
