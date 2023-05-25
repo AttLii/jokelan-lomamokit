383f243f57fa8526eab09e5f1cfa9b452c69c76c
Atte Liimatainen <atte.liimat@gmail.com>
2023-05-25 20:45:28 +0300

    add poc

---
+++ src/pages/api/draft.ts
@@ -0,0 +1,22 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import previewClient from "../../factories/PreviewClient";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (
+    req.query.secret !== process.env.DRAFT_SECRET ||
+    typeof req.query.path !== "string"
+  ) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
+
+  const content = await previewClient.getContentByPath(req.query.path);
+  if (!content) {
+    return res.status(401).json({ message: "Invalid path" });
+  }
+
+  // Redirect the user back to the page they're previewing.
+  res.setHeader("Content-Type", "text/html");
+  res.setDraftMode({ enable: true });
+  res.redirect(302, req.query.path);
+};
+export default handler;
