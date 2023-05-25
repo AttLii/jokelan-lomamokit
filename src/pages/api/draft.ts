c169aba9ac837a95c5a09cf5fedb11436266333d
Atte Liimatainen <atte.liimat@gmail.com>
2023-05-25 20:47:48 +0300

    fix imports

--- src/pages/api/draft.ts
+++ src/pages/api/draft.ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import previewClient from "../../factories/PreviewClient";
+import previewClient from "../../factories/previewClient";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (
